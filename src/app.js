ef0ee20a12495761ea3e11da30210d4759dbc331
Wisam Halabi <wisamwrh@gmail.com>
2020-09-04 18:22:08 +0300

    Preparing clientside links to serverside and starting on the webpages

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 require('./db/mongoose')
-
-
-
 const app = express()
 
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
 app.use(express.json())
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req , res) => {
+    res.render('index',{
+    })
+})
 
 
 module.exports = app

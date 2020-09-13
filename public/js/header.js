a7c531c72e56abd09b5292a3e2a39160afe65890
Wisam Halabi <wisamwrh@gmail.com>
2020-09-13 21:37:58 +0300

    Fixed about and added 404 page

--- public/js/header.js
+++ public/js/header.js
@@ -1,9 +1,26 @@
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown2").classList.remove("show");
   }
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+    document.getElementById("myDropdown").classList.remove("show");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn2')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content2");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];

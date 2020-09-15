5c8d538e3a9588d549e897281775463deb91f220
Wisam Halabi <wisamwrh@gmail.com>
2020-09-15 23:35:38 +0300

    Added a filler message to a coach that has no clients , fixed the buttons UI at coachClients

--- public/js/coachClients.js
+++ public/js/coachClients.js
@@ -13,13 +13,19 @@ $(document).ready(async () => {
         // body data type must match "Content-Type" header
     });
     getClients.json().then((data) => {
-        data.forEach(client => {
-            $('#clientsList').append(' <div class="clientDIV"><div class="pic-DIV"><div class="pic"></div></div><div class="client-info-DIV"> <div class="client-info-row-DIV"> <div class="client-info-text">Name:</div><div class="client-info-data">' + client.name + '</div><div class="client-info-text">Age:</div> <div class="client-info-data">' + client.age + '</div></div><div class="client-info-row-DIV"> <div class="client-info-text">Weight:</div><div class="client-info-data">' + client.weight + ' KG</div><div class="client-info-text">Height:</div><div class="client-info-data">' + client.height + ' cm</div> </div> </div><div class="coach-select-btn-DIV"><button value="' + client.id + '"  class="coach-select-btn">Update Schedule & Nutritions</button></div></div><div class="clientDIV-filler"></div>')
-        });
+        console.log(data.length)
+        if (data.length !== 0) {
+            $('#noClientsmsg').css('display','none')
+            data.forEach(client => {
+                $('#clientsList').append(' <div class="clientDIV"><div class="pic-DIV"><div class="pic"></div></div><div class="client-info-DIV"> <div class="client-info-row-DIV"> <div class="client-info-text">Name:</div><div class="client-info-data">' + client.name + '</div><div class="client-info-text">Age:</div> <div class="client-info-data">' + client.age + '</div></div><div class="client-info-row-DIV"> <div class="client-info-text">Weight:</div><div class="client-info-data">' + client.weight + ' KG</div><div class="client-info-text">Height:</div><div class="client-info-data">' + client.height + ' cm</div> </div> </div><div class="coach-select-btn-DIV"><button value="' + client.id + '"  class="coach-select-btn">Update Schedule & Nutritions</button></div></div><div class="clientDIV-filler"></div>')
+            });
+        } else {
+            $('#noClientsmsg').css('display','flex') 
+        }
     });
     $("#clientsList").on("click", "button", async (e) => {
-        var clientID =  $(e.target).attr("value")
-        window.location.href = ("/coachScheduleUpdate?id="+clientID)
+        var clientID = $(e.target).attr("value")
+        window.location.href = ("/coachScheduleUpdate?id=" + clientID)
     });
 });
 

af114bead9bead49ed19991763a0689483729bd9
Wisam Halabi <wisamwrh@gmail.com>
2020-09-18 07:24:27 +0300

    Fixing some css.

--- public/js/header.js
+++ public/js/header.js
@@ -1,30 +1,48 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-  document.getElementById("myDropdown2").classList.remove("show");
-}
-function myFunction2() {
-  document.getElementById("myDropdown2").classList.toggle("show");
-  document.getElementById("myDropdown").classList.remove("show");
-}
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+$('#dropbtn2').click(() => {
+  if ($('#myDropdown2').css('display') == 'flex') {
+    $('#myDropdown2').css('display', 'none')
+  } else {
+    $('#myDropdown2').css('display', 'flex')
+    $('#myDropdown').css('display', 'none')
   }
-  if (!event.target.matches('.dropbtn2')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content2");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+
+})
+$('#dropbtn').click(() => {
+  if ($('#myDropdown').css('display') == 'flex') {
+    $('#myDropdown').css('display', 'none')
   }
-}
+  else {
+    $('#myDropdown').css('display', 'flex')
+    $('#myDropdown2').css('display', 'none')
+  }
+})
+$(window).click(function (e) {
+  if (!e.target.matches('#dropbtn') && !e.target.matches('#dropbtn2')
+    && !e.target.matches('#coach-sign-up-btn') && !e.target.matches('#client-sign-up-btn')
+    && !e.target.matches('#coach-log-in-btn') && !e.target.matches('#client-log-in-btn')) {
+    $('#myDropdown').css('display', 'none')
+    $('#myDropdown2').css('display', 'none')
+  }
+})
+// window.onclick = function (event) {
+//   if (!event.target.matches('#dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+//   if (!event.target.matches('#dropbtn2')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content2");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
